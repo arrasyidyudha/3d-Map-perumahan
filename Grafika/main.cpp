563f38b3f64f13b96e8cacfda1c7365eb78371d4
Rasyid <48144996+arrasyidyudha@users.noreply.github.com>
2019-11-22 14:52:45 +0700

    Update main.cpp

--- Grafika/main.cpp
+++ Grafika/main.cpp
@@ -12,8 +12,10 @@ void keyboard(unsigned char, int, int);
 void resize(int, int);
 void house(float,float,float);
 void mall(float,float,float);
+void apartemen(float,float,float);
 void car(float,float,float);
 void circle1(float);
+
 
 float  h=5,h1=6,d1=4,g=1,g1=2;
 
@@ -235,10 +237,216 @@ glBegin(GL_POLYGON);
  	glVertex3f(x,y+100,z+45);
 	glEnd();
 }
+
+void apartemen(float x,float y,float z)
+{
+	int i;
+	int j;
+	glColor3f(0.7,0.7,0.7);
+	glBegin(GL_POLYGON);
+	glVertex3f(x,y,z+0.5);
+	glVertex3f(x+45,y,z+0.5);
+	glVertex3f(x+45,y+100,z+0.5);
+ 	glVertex3f(x,y+100,z+0.5);
+	glEnd();  glColor3f(0.8,0.8,0.8);
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(11*i,0,0);
+	glBegin(GL_POLYGON);
+	glVertex3f(x+2,y+98,z);
+	glVertex3f(x+10,y+98,z);
+	glVertex3f(x+10,y+88,z);
+ 	glVertex3f(x+2,y+88,z);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}glColor3f(0,0,0);
+for(j=0;j<8;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(11*i,0,0);
+		glBegin(GL_LINE_LOOP);
+	glVertex3f(x+2,y+98,z);
+	glVertex3f(x+10,y+98,z);
+	glVertex3f(x+10,y+88,z);
+ 	glVertex3f(x+2,y+88,z);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}
+
+glColor3f(0.6,0.6,0.6);
+	glBegin(GL_POLYGON);
+	glVertex3f(x,y,z+45-0.5);
+	glVertex3f(x+45,y,z+45-0.5);
+	glVertex3f(x+45,y+100,z+45-0.5);
+ 	glVertex3f(x,y+100,z+45-0.5);
+	glEnd();  	glColor3f(0.8,0.8,0.8);
+
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(11*i,0,0);
+	glBegin(GL_POLYGON);
+	glVertex3f(x+2,y+98,z+45);
+	glVertex3f(x+10,y+98,z+45);
+	glVertex3f(x+10,y+88,z+45);
+ 	glVertex3f(x+2,y+88,z+45);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}glColor3f(0,0,0);
+
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(11*i,0,0);
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(x+2,y+98,z+45);
+	glVertex3f(x+10,y+98,z+45);
+	glVertex3f(x+10,y+88,z+45);
+ 	glVertex3f(x+2,y+88,z+45);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}
+
+glColor3f(0.6,0.6,0.6);
+	glBegin(GL_POLYGON);
+	glVertex3f(x+0.5,y,z);
+	glVertex3f(x+0.5,y,z+45);
+	glVertex3f(x+0.5,y+100,z+45);
+ 	glVertex3f(x+0.5,y+100,z);
+	glEnd();
+	glColor3f(0.8,0.8,0.8);
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(0,0,11*i);
+	glBegin(GL_POLYGON);
+	glVertex3f(x,y+98,z+2);
+	glVertex3f(x,y+98,z+10);
+	glVertex3f(x,y+88,z+10);
+ 	glVertex3f(x,y+88,z+2);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}glColor3f(0,0,0);
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(0,0,11*i);
+		glBegin(GL_LINE_LOOP);
+	glVertex3f(x,y+98,z+2);
+	glVertex3f(x,y+98,z+10);
+	glVertex3f(x,y+88,z+10);
+ 	glVertex3f(x,y+88,z+2);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}
+
+glColor3f(0.6,0.6,0.6);
+
+	glBegin(GL_POLYGON);
+	glVertex3f(x+45-0.5,y,z-0.5);
+	glVertex3f(x+45-0.5,y,z+45-0.5);
+	glVertex3f(x+45-0.5,y+100,z+45-0.5);
+ 	glVertex3f(x+45-0.5,y+100,z-0.5);
+	glEnd(); glColor3f(0.8,0.8,0.8);
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(0,0,11*i);
+	glBegin(GL_POLYGON);
+	glVertex3f(x+45,y+98,z+2);
+	glVertex3f(x+45,y+98,z+10);
+	glVertex3f(x+45,y+88,z+10);
+ 	glVertex3f(x+45,y+88,z+2);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}
+glColor3f(0,0,0);
+for(j=0;j<5;j++)
+{
+
+	glPushMatrix();
+		glTranslatef(0,-12*j,0);
+	for(i=0;i<4;i++)
+	{
+		glPushMatrix();
+		glTranslatef(0,0,11*i);
+		glBegin(GL_LINE_LOOP);
+	glVertex3f(x+45,y+98,z+2);
+	glVertex3f(x+45,y+98,z+10);
+	glVertex3f(x+45,y+88,z+10);
+ 	glVertex3f(x+45,y+88,z+2);
+	glEnd();
+	glPopMatrix();
+	}glPopMatrix();
+}
+
+glColor3f(0.5,0.5,0.5);
+glBegin(GL_POLYGON);
+	glVertex3f(x,y,z-0.5);
+	glVertex3f(x+45,y,z-0.5);
+	glVertex3f(x+45,y,z+45-0.5);
+ 	glVertex3f(x,y,z+45-0.5);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3f(x,y+100,z);
+	glVertex3f(x+45,y+100,z);
+	glVertex3f(x+45,y+100,z+45);
+ 	glVertex3f(x,y+100,z+45);
+	glEnd();
+
+
+}
+
+
+
 void house(float x,float y,float z)
 {
     //warna dinding samoing kanan
-	glColor3f(0.1,0.1,0.1);
+	glColor3f(0.2,2.3,1.7);
 	glBegin(GL_POLYGON);
 	glVertex3f(x,y,z);
 	glVertex3f(x,y,z+30);
@@ -246,7 +454,7 @@ void house(float x,float y,float z)
 	glVertex3f(x,y+15,z);
 	glEnd();
     // warna dinding samping kiri
-	glColor3f(2,2,2);
+	glColor3f(0.2,2.3,1.7);
 	glBegin(GL_POLYGON);
 	glVertex3f(x-15,y,z);
 	glVertex3f(x-15,y+15,z);
@@ -254,7 +462,7 @@ void house(float x,float y,float z)
 	glVertex3f(x-15,y,z+30);
 	glEnd();
     //warna dinding depan
-	glColor3f(0.1,0.1,0.1);
+	glColor3f(2.4,3.4,0.4);
 	glBegin(GL_POLYGON);
 	glVertex3f(x,y,z+30);
 	glVertex3f(x-15,y,z+30);
@@ -262,7 +470,7 @@ void house(float x,float y,float z)
 	glVertex3f(x,y+15,z+30);
 	glEnd();
 // warna dinding belakang
-	glColor3f(0.4,0.4,0.4);
+	glColor3f(2.4,3.4,0.4);
 	glBegin(GL_POLYGON);
 	glVertex3f(x-15,y,z);
 	glVertex3f(x-15,y+15,z);
@@ -297,20 +505,22 @@ void house(float x,float y,float z)
 	glEnd();
 
 //warna atap depan dan belakang
-glColor3f(1,1,1);
+glColor3f(2.4,3.4,0.4);
 	glBegin(GL_POLYGON);
 	glVertex3f(x,y+15,z);
 	glVertex3f(x-15,y+5,z);
 	glVertex3f(x-2.5*3,y+7.5*3,z);
 	glEnd();
 
-glColor3f(1,1,1);
+glColor3f(2.4,3.4,0.4);
 	glBegin(GL_POLYGON);
 	glVertex3f(x,y+15,z+30);
 	glVertex3f(x-15,y+15,z+30);
 	glVertex3f(x-2.5*3,y+7.5*3,z+30);
 	glEnd();
 }
+
+
 
 
 
@@ -857,7 +1067,7 @@ void house1()
     mall(100,0,-120);
 
     car(60,0,-90);
-    car(40,0,60);
+    apartemen(-140,0,-90);
 
 
 
